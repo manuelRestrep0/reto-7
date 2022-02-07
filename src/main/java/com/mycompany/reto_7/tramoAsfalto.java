7058baba049817e86501bc75bed3d4d163ba851e
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2022-02-07 10:54:00 -0600

    Add files via upload

--- src/main/java/com/mycompany/reto_7/tramoAsfalto.java
+++ src/main/java/com/mycompany/reto_7/tramoAsfalto.java
@@ -1,7 +1,14 @@
 package com.mycompany.reto_7;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class tramoAsfalto extends tramoGenerico {
     String nombreProveedor;
     double velocidadMaxima;
     boolean permitirAdelantamiento;
+    Map<Float,Float> coordenadasIniciales = new Hashtable();
+    Map<Float,Float> coordenadasFinales = new Hashtable();
+
+
 }
--- src/main/java/com/mycompany/reto_7/tramoSinAsfaltar.java
+++ src/main/java/com/mycompany/reto_7/tramoSinAsfaltar.java
@@ -1,7 +1,12 @@
 package com.mycompany.reto_7;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class tramoSinAsfaltar extends tramoGenerico {
     String tipoMaterial;
     double espesorMaterial;
+    Map<Float,Float> coordenadasIniciales = new Hashtable();
+    Map<Float,Float> coordenadasFinales = new Hashtable();
 
 }
